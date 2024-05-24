9d8f0ca92707ae786fecc11e3acb3107035c6d0b
boithanh <boithanh01694@gmail.com>
2024-05-24 18:19:47 +0700

    complete animation header menu

--- js/index.js
+++ js/index.js
@@ -1,20 +1,18 @@
-getElement(".header__navbar").addEventListener("click", () => {
-    const headerMenu = getElement(".header__menu");
-    if (headerMenu.classList == "header__menu hide") {
-        headerMenu.classList = "header__menu show";
+const getElement = (selector) => {
+    return document.querySelector(selector);
+}
+
+getElement(".header_navbar").addEventListener("click", () => {
+    const headerMenu = getElement(".header_menu");
+    if (headerMenu.classList == "header_menu hide") {
+        headerMenu.classList = "header_menu show";
     } else {
-        headerMenu.classList = "header__menu hide";
+        headerMenu.classList = "header_menu hide";
     }
-    const headerLogo = getElement(".header__logo");
-    if (headerLogo.classList == "header__logo hide") {
-        headerLogo.classList = "header__logo show";
+    const headerNav = getElement(".header_navbar");
+    if (headerNav.classList == "header_navbar hide") {
+        headerNav.classList = "header_navbar show";
     } else {
-        headerLogo.classList = "header__logo hide";
-    }
-    const headerNav = getElement(".header__navbar");
-    if (headerNav.classList == "header__navbar hide") {
-        headerNav.classList = "header__navbar show";
-    } else {
-        headerNav.classList = "header__navbar hide";
+        headerNav.classList = "header_navbar hide";
     }
 })
